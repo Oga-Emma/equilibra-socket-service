09e4482f8e3a57855afcb75187b1dce7751a8a61
Oga-Emma <sevenapps2018@gmail.com>
2020-11-12 11:58:11 +0100

    debuging socket error

--- socket-incoming.js
+++ socket-incoming.js
@@ -6,36 +6,47 @@ socket.on('connect', function () {
 });
 
 socket.on('topic-changed', (payload) => {
+    console.log(payload);
     outgoing.emitTopicChange(payload);
 });
 socket.on('comment', (payload) => {
+    console.log(payload);
     outgoing.emitNewComment(payload);
 });
 socket.on('account-suspended', (payload) => {
+    console.log(payload);
     outgoing.emitAccountSuspended(payload);
 });
 socket.on('join-room', (payload) => {
+    console.log(payload);
     outgoing.emitJoinRoom(payload)
 });
 socket.on('leave-room', (payload) => {
+    console.log(payload);
     outgoing.emitLeaveRoom(payload);
 });
 socket.on('vote-topic-change', (payload) => {
+    console.log(payload);
     outgoing.emitTopicChangeVote(payload);
 });
 socket.on('vote-topic-closed', (payload) => {
+    console.log(payload);
     outgoing.emitTopicChangeVoteEnd(payload);
 });
 socket.on('topic-discussion-vote', (payload) => {
+    console.log(payload);
     outgoing.emitDiscussionVote(payload);
 });
 socket.on('close-discussion-vote', (payload) => {
+    console.log(payload);
     outgoing.emitDiscussionVoteEnd(payload)
 });
 socket.on('new-vote', (payload) => {
+    console.log(payload);
     outgoing.vote(payload);
 });
 socket.on('refresh-account', (payload) => {
+    console.log(payload);
     outgoing.refreshAccount(payload)
 });
 
